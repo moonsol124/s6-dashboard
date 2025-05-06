56d284979e5024ad5a507cdc5cf57fe6c376fbcb
moonsol124 <moonsol124@gmail.com>
2025-05-06 12:42:09 +0200

    f

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx'; // Import AuthProvider
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
