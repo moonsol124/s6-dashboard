e459322a1943383db3e10136640164449cefe135
moonsol124 <moonsol124@gmail.com>
2025-05-08 22:32:04 +0200

    f

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,14 @@ const repoName = 's6-dashboard'; // <<<--- CHANGE THIS
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
-  const base = command === 'build' ? `/${repoName}/` : '/'; // Should resolve to '/s6-dashboard/' for build
+  const base = command === 'build' ? `/${repoName}/` : '/';
   return {
     plugins: [react()],
     base: base,
     server: {
       host: '0.0.0.0',
-      port: 10000
+      port: 10000,
+      allowedHosts: ['s6-dashboard.onrender.com'] // 👈 Add this line
     }
   }
 })
